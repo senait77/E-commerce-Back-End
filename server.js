bd2f81d266ac6909622f46f0c36a42b479b9ce56
Senait <senutekie77@gmail.com>
2021-05-13 11:59:23 -0700

    server has started on port 3001

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes =  require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(routes);
+
+app.listen(PORT,()=> {
+    console.log(`App is listening on port ${PORT}!`);
+});
